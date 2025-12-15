4bf8c1c7ed59de167ba0cc3c090731a07013e546
diegoDiaz37 <diego.diaz5671@gmail.com>
2025-12-15 13:56:00 +0100

    Something in the wayyy

--- algorithms.py
+++ algorithms.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.tree import DecisionTreeRegressor
@@ -22,7 +23,20 @@ def entrenar_modelo(path_csv, algoritmo="Árbol de decisión"):
     """
     df = pd.read_csv(path_csv, sep=";")
     target = "intensidad"
-    X = df.drop(columns=[target, "fecha"], errors="ignore")  # Ignorar si no existe columna fecha
+
+    df["fecha"] = pd.to_datetime(df["fecha"], dayfirst=True)
+    df["hora"] = df["fecha"].dt.hour
+    df["hora_sin"] = np.sin(2 * np.pi * df["hora"] / 24)
+    df["hora_cos"] = np.cos(2 * np.pi * df["hora"] / 24)
+
+    df["dia_semana"] = df["fecha"].dt.dayofweek
+    #df["es_finde"] = (df["dia_semana"] >= 5).astype(int)
+    df["dia_semana"] = df["dia_semana"].astype(str)
+
+    #df["mes"] = df["fecha"].dt.month.astype(str)
+    #df["hora_x_finde"] = df["hora"] * df["es_finde"]
+
+    X = df.drop(columns=[target, "fecha", "hora", "conductores_hombres", "conductores_mujeres", "conductores_desconocidos", "Edad_desconocida", "De 10 a 17 años", "De 18 a 24 años", "De 25 a 34 años", "De 35 a 44 años", "De 45 a 54 años", "De 55 a 64 años", "De 65 a 74 años", "Más de 74 años", "Lesividad_leve", "Lesividad_grave", "Lesividad_fallecido", "Lesividad_desconocido", "Turismo", "Motocicletas", "Furgonetas", "Bicicletas", "Vehiculos_especiales", "longitud", "latitud", "sealevelpressure", "solarradiation", "solarenergy", "uvindex", "carga", "ocupacion", "vmed", "periodo_integracion"], errors="ignore")  # Ignorar en caso no existan las columnas
     y = df[target]
 
     # Columnas categóricas y numéricas
@@ -39,15 +53,26 @@ def entrenar_modelo(path_csv, algoritmo="Árbol de decisión"):
 
     # Pipelines de modelos
     modelos = {
-        "Árbol de decisión": Pipeline([
-            ("pre", preprocesamiento),
-            ("model", DecisionTreeRegressor(random_state=42, max_depth=5, min_samples_leaf=5))
-        ]),
+        "Árbol de decisión": GridSearchCV(
+            Pipeline([
+                ("pre", preprocesamiento),
+                ("model", DecisionTreeRegressor(random_state=42))
+            ]),
+            param_grid={
+                "model__max_depth": [None, 5, 10, 20],
+                "model__min_samples_leaf": [1, 2, 5, 10],
+                "model__min_samples_split": [2, 5, 10]
+            },
+            cv=5,
+            scoring="neg_mean_squared_error",
+            n_jobs=-1
+        ),
         "Random Forest": Pipeline([
             ("pre", preprocesamiento),
             ("model", RandomForestRegressor(
-                n_estimators=50,
-                max_depth=10,
+                n_estimators=200,
+                max_depth=None,
+                min_samples_leaf=2,
                 max_features="sqrt",
                 n_jobs=-1,
                 random_state=42
